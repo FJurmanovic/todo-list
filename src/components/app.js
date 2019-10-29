c788da86decbc23641ca4330d0804e45631ece6e
Fran Jurmanović <fjurma12@outlook.com>
2019-10-29 20:35:04 +0100

    Added function for adding new items to list from inputView after button press

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,63 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      padding: '2px 4px',
+      display: 'flex',
+      alignItems: 'center',
+      width: 400,
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+}));
+
+const todolist = [
+  {id: 1, todo: "First Todo"},
+  {id: 2, todo: "Second Todo"},
+  {id: 3, todo: "Welcome to the jungle, cuz this ain't sahara desert"}
+];
+
+function Add(){
+  var textvalue;
+  function handleChange(e) {
+    textvalue = e.target.value;
+  }
+
+  function addFunc(){
+    todolist.push({id: todolist.length +1, todo: textvalue});
+    console.log(todolist);
+  }
+  const classes = useStyles();
+  return(
+    <Paper className={classes.root}>
+      <InputBase
+        className={classes.input}
+        placeholder="Add new todo"
+        inputProps={{ 'aria-label': 'add todo' }}
+        onChange={handleChange}
+      />
+      <IconButton className={classes.iconButton} aria-label="add" onClick={addFunc}>
+        <AddIcon />
+      </IconButton>
+    </Paper>
+  );
+}
 
 function Todo(props) {
     const content = props.todolist.map((todolist) =>
@@ -16,6 +72,7 @@ function Todo(props) {
         </IconButton>
       </div>
     );
+    
     return (
       <div>
         {content}
@@ -23,16 +80,14 @@ function Todo(props) {
     );
   }
 
-const todolist = [
-    {id: 1, todo: "First Todo"},
-    {id: 2, todo: "Second Todo"},
-    {id: 3, todo: "Welcome to the jungle, cuz this ain't sahara desert"}
-];
 
 class App extends Component {
     render() {
         return(
-            <Todo todolist={todolist} />
+            <div>
+                <Add />
+                <Todo todolist={todolist} />
+            </div>
         );
     }
 }
