9dfcb9981b00d420fe10d214d64f44db53c09ace
Fran Jurmanović <fjurma12@outlook.com>
2019-10-30 22:42:06 +0100

    Switched functions with classes && adding new todos works

--- src/components/app.js
+++ src/components/app.js
@@ -7,86 +7,94 @@ import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import AddIcon from '@material-ui/icons/Add';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      padding: '2px 4px',
-      display: 'flex',
-      alignItems: 'center',
-      width: 400,
-    },
-    input: {
-      marginLeft: theme.spacing(1),
-      flex: 1,
-    },
-    iconButton: {
-      padding: 10,
-    },
-    divider: {
-      height: 28,
-      margin: 4,
-    },
-}));
+import './app.css';
 
-const todolist = [
-  {id: 1, todo: "First Todo"},
-  {id: 2, todo: "Second Todo"},
-  {id: 3, todo: "Welcome to the jungle, cuz this ain't sahara desert"}
-];
 
-function Add(){
-  var textvalue;
-  function handleChange(e) {
-    textvalue = e.target.value;
+class TodoItem extends React.Component {
+  render(){
+    return(
+      <div key={this.props.id}>
+            <Checkbox
+                value="checkedA"
+                inputProps={{ 'aria-label': 'Checkbox A' }}
+            />
+            <span>{this.props.todo}</span>
+            <IconButton aria-label="delete">
+                <DeleteIcon fontSize="small" />
+            </IconButton>
+          </div>
+    );
   }
-
-  function addFunc(){
-    todolist.push({id: todolist.length +1, todo: textvalue});
-    console.log(todolist);
-  }
-  const classes = useStyles();
-  return(
-    <Paper className={classes.root}>
-      <InputBase
-        className={classes.input}
-        placeholder="Add new todo"
-        inputProps={{ 'aria-label': 'add todo' }}
-        onChange={handleChange}
-      />
-      <IconButton className={classes.iconButton} aria-label="add" onClick={addFunc}>
-        <AddIcon />
-      </IconButton>
-    </Paper>
-  );
 }
 
-function Todo(props) {
-    const content = props.todolist.map((todolist) =>
-      <div key={todolist.id}>
-        <Checkbox
-            value="checkedA"
-            inputProps={{ 'aria-label': 'Checkbox A' }}
-        />
-        <span>{todolist.todo}</span>
-        <IconButton aria-label="delete">
-            <DeleteIcon fontSize="small" />
-        </IconButton>
-      </div>
-    );
-    
-    return (
+class Todo extends React.Component{
+    render(){
+      return(
+        <div className="ToDo-Content">
+            {this.props.todolist.map((item, key) => {
+              return <TodoItem
+                key={key}
+                todo={item.todo}
+              />
+            }
+          )}
+        </div>
+      );
+    }
+  };
+  
+class Add extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todolist: [
+        {id: 1, todo: "First Todo"},
+        {id: 2, todo: "Second Todo"},
+        {id: 3, todo: "Welcome to the jungle, cuz this ain't sahara desert"}
+      ],
+      textvalue: "",
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState ( {
+      textvalue: e.target.value,
+    });
+  }
+
+  addFunc(){
+      this.setState (prevState => (
+        {
+          todolist: [...prevState.todolist, {id: this.state.todolist.length +1, todo: this.state.textvalue}]
+        }
+      ));
+      console.log(this.state.todolist);
+  }
+  render(){
+    return(
       <div>
-        {content}
+        <Paper className="root">
+          <InputBase
+            className="input"
+            placeholder="Add new todo"
+            inputProps={{ 'aria-label': 'add todo' }}
+            onChange={this.handleChange}
+          />
+          <IconButton className="iconButton" aria-label="add" onClick={() => this.addFunc()}>
+            <AddIcon />
+          </IconButton>
+        </Paper>
+        <Todo todolist={this.state.todolist} />
       </div>
     );
   }
+}
 
-
-class App extends Component {
+class App extends React.Component {
     render() {
         return(
             <div>
                 <Add />
-                <Todo todolist={todolist} />
             </div>
         );
     }
